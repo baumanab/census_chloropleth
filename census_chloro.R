38b85914c86a7d50ee88db707fff47ca0f37a3a7
baumanab <baumanab@gmail.com>
2016-11-12 15:31:18 -0800

    plot county level data

--- census_chloro.R
+++ census_chloro.R
@@ -1,8 +1,25 @@
 
 
+# load libraries
+
 library(choroplethr)
+library(tidyr)
+library(dplyr)
+
+
+# TODO Take a glimpse
+
+# state level data
 
 data(df_pop_state)
 head(df_pop_state)
 
 state_choropleth(df_pop_state)
+
+# county level data
+
+data(df_pop_county)
+head(df_pop_county)
+
+county_choropleth(df_pop_county)
+
