08c6ad6f7858c12cbc92f85065888f73473944ea
baumanab <baumanab@gmail.com>
2016-11-14 19:59:51 -0800

    state level population data chloro

--- census_chloro.R
+++ census_chloro.R
@@ -5,6 +5,7 @@
 library(choroplethr)
 library(tidyr)
 library(dplyr)
+library(acs)
 
 
 # TODO Take a glimpse
@@ -22,4 +23,25 @@ data(df_pop_county)
 head(df_pop_county)
 
 county_choropleth(df_pop_county)
+
+# explore state population and demographics data
+
+?df_pop_state
+
+?df_state_demographics
+
+data(df_state_demographics)
+
+?get_state_demographics
+
+df_2010_demographics = get_state_demographics(2010)
+
+# Creating a map of 2010 population estimates:
+  
+df_2010_demographics$value = df_2010_demographics$total_population
+
+state_choropleth(df_2010_demographics, 
+                 title= '2010 State Population Estimates',
+                 legend= 'Population')
+
 
